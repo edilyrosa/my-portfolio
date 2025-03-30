c306f93a40ba042a580cd0aac96692b3223f334e
Edily Mora <edilyrosa@gmail.com>
2025-03-30 12:29:33 -0400

    testing 03

--- src/components/FloatingDockDemo.jsx
+++ src/components/FloatingDockDemo.jsx
@@ -1,3 +1,58 @@
+// import { FloatingDock } from "../components/ui/floating-dock"
+// import { IconBrandGithub, IconBrandX, IconExchange, IconHome, IconNewSection, IconTerminal2 } from "@tabler/icons-react"
+// import Image from "next/image"
+
+// export default function FloatingDockDemo() {
+//   const links = [
+//     {
+//       title: "Home",
+//       icon: <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+//       href: "#",
+//     },
+//     {
+//       title: "Products",
+//       icon: <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+//       href: "#",
+//     },
+//     {
+//       title: "Components",
+//       icon: <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+//       href: "#",
+//     },
+//     {
+//       title: "Aceternity UI",
+//       icon: <Image src="/assets/diez.png" alt="Aceternity Logo"/>,
+//       href: "#",
+//     },
+//     {
+//       title: "Changelog",
+//       icon: <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+//       href: "#",
+//     },
+//     {
+//       title: "Twitter",
+//       icon: <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+//       href: "#",
+//     },
+//     {
+//       title: "GitHub",
+//       icon: <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+//       href: "#",
+//     },
+//   ]
+
+//   return (
+//     <div className="relative w-full">
+//       <FloatingDock items={links} />
+
+      
+//     </div>
+//   )
+// }
+
+
+
+
 import { FloatingDock } from "../components/ui/floating-dock"
 import { IconBrandGithub, IconBrandX, IconExchange, IconHome, IconNewSection, IconTerminal2 } from "@tabler/icons-react"
 import Image from "next/image"
@@ -21,7 +76,11 @@ export default function FloatingDockDemo() {
     },
     {
       title: "Aceternity UI",
-      icon: <Image src="/assets/diez.png" alt="Aceternity Logo"/>,
+      icon: (
+        <div className="relative w-full h-full ml-[-8px]">
+          <Image src="/assets/diez.png" alt="Aceternity Logo" fill className="object-cover" sizes="100%" priority />
+        </div>
+      ),
       href: "#",
     },
     {
@@ -44,8 +103,6 @@ export default function FloatingDockDemo() {
   return (
     <div className="relative w-full">
       <FloatingDock items={links} />
-
-      
     </div>
   )
 }
